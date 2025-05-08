76016fadc40b649230e7ccde13c3157f78f18a0c
awatersny <awatersny@gmail.com>
2025-05-08 14:35:01 -0400

    Add seed routes for jobs and companies

--- data/seed.mjs
+++ data/seed.mjs
@@ -1,16 +1,29 @@
 export const jobData = [
   {
-    title: "",
-    description: "",
-    skills: [],
-    status: ""
+    title: "Software Engineer (Entry to Mid-Level)",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestias error necessitatibus debitis facilis? Ut illo quod expedita, unde ea amet voluptatem eaque pariatur officiis dolore excepturi quibusdam? Vero, voluptatem.",
+    skills: ["REST APIs", "HTTP", "HTML", "CSS", "Python", "Javascript", "React", "Django", "Communication",  "Collaboration ",  "Problem Solving "],
+    status: "applied"
+  },
+  {
+    title: "Frontend Developer",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestias error necessitatibus debitis facilis? Ut illo quod expedita, unde ea amet voluptatem eaque pariatur officiis dolore excepturi quibusdam? Vero, voluptatem.",
+    skills: ["REST APIs", "HTTP", "HTML", "CSS", "Python", "Javascript", "React", "Django", "Communication",  "Collaboration ",  "Problem Solving "],
+    status: "applied"
   }
 ]
 
 export const companyData = [
   {
-    name: "",
-    description: "",
-    values: []
+    name: "Funnel Leasing",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestias error necessitatibus debitis facilis? Ut illo quod expedita, unde ea amet voluptatem eaque pariatur officiis dolore excepturi quibusdam? Vero, voluptatem.",
+    values: ["Everyone sweeps the floor.", "Do the right thing always and in all ways.", "Change with purpose.", "The will to win"],
+    website: "https://funnel-leasing.breezy.hr/"
+  },
+  {
+    name: "Publicis Group",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestias error necessitatibus debitis facilis? Ut illo quod expedita, unde ea amet voluptatem eaque pariatur officiis dolore excepturi quibusdam? Vero, voluptatem.",
+    values: ["Clients come first", "We are seamless", "We are frictionless", "We are modular", "We are united"],
+    website: "https://www.publicisgroupe.com/fr/splash"
   }
 ]
