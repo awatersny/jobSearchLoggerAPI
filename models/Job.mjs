d5643acf1a83ec5d0ed7936a9d0a01fb84468ebc
awatersny <awatersny@gmail.com>
2025-05-08 10:22:24 -0400

    Add job model

---
+++ models/Job.mjs
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["applied", "interviewing", "offered", "rejected"],
+      required: true
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      default: null
+    },
+  }
+)
+
+const Job = mongoose.model("Job", jobSchema)
+
+export default Job
