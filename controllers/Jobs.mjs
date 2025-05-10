68227f9a6849d434af1cd3141f309e5de184a055
awatersny <awatersny@gmail.com>
2025-05-10 17:50:38 -0400

    Update relational routes between jobs and companies

--- controllers/Jobs.mjs
+++ controllers/Jobs.mjs
@@ -1,5 +1,6 @@
 import Job from "../models/Job.mjs"
 import { jobData } from "../data/seed.mjs"
+import Company from "../models/Company.mjs"
 
 export async function getAllJobs(req, res) {
   try {
@@ -44,8 +45,15 @@ export async function getJob(req, res) {
 
 export async function removeJob(req, res) {
   try {
+    const job = await Job.findById(req.params.id)
+    const company = job.company ? await Company.findById(job.company._id) : {msg: "No company assigned"}
+    const idx = company.jobs ? company.jobs.indexOf(req.params.id) : -1
     await Job.findByIdAndDelete(req.params.id)
-    res.json({removed: `Job Id ${req.params.id}`})
+    if(idx > -1) {
+      company.jobs.splice(idx, 1)
+      company.save() 
+    }
+    res.json([{removed: `Job Id ${req.params.id}`}, company])
   } catch (error) {
     res.json({error: error})
   }
